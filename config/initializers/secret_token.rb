b155cbc56d5fe60080a37315cb615fdf4e89974f
Evan <egtann@gmail.com>
2012-01-07 12:06:14 -0500

    Good progress on homepage, lightbox installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecclesia::Application.config.secret_token = '9ad1b24b8bdc31dd5b39a55662fe85240c24f4132412529231568c26eea95effb39a80ad40438fd6775ef655d52cb73cfd5f92f8d8764845688a56f7605a94a6'
